fc7da2db69d118054cde2d77d07c4c203553e1b4
un1u3 <python.github.devs@gmail.com>
2025-09-07 19:52:21 +0545

    added

--- financeV1/data/db.py
+++ financeV1/data/db.py
@@ -18,3 +18,30 @@ def init_db():
     ''')
     conn.commit()
     conn.close()
+
+
+def add_trasaction(tx):
+    conn = sqlite3.connect(DB_FILE)
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO transactions (type, amount, category, date, note) VALUES (?, ?, ?, ?, ?)",
+        (tx.__class__.__name__, str(tx.amount), tx.category, tx.date.strftime("%Y-%m-%d"), tx.note)
+    )
+    conn.commit()
+    conn.close()
+
+
+def get_transactions():
+    conn = sqlite3.connect(DB_FILE)
+    cursor = conn.cursor()
+    cursor.execute("SELECT id, type, amount, category, date, note FROM transactions ORDER BY date DESC")
+    rows = cursor.fetchall()
+    conn.close()
+    return rows
+
+def delete_transaction(tx_id):
+    conn = sqlite3.connect(DB_FILE)
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM transactions WHERE id=?", (tx_id,))
+    conn.commit()
+    conn.close()
+
